ca2dea08aec5650468060167df89b57ab267b8af
hisham <hishamaitrich@gmail.com>
2024-08-23 16:08:29 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/scoreboardapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scoreboardapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:scoreboardapp/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
