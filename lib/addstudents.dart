f029b9606c5a4aaa079ff78b21e8b152ec35d5a7
hishamponnath <hishamaitrich@gmail.com>
2024-08-25 19:55:27 +0530

    add students and view students with firebase

--- lib/addstudents.dart
+++ lib/addstudents.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class addstd extends StatefulWidget {
@@ -8,6 +9,22 @@ class addstd extends StatefulWidget {
 }
 
 class _addstdState extends State<addstd> {
+  final CollectionReference students =
+      FirebaseFirestore.instance.collection('students');
+
+  TextEditingController stdname = TextEditingController();
+  TextEditingController stdcourse = TextEditingController();
+  TextEditingController stdscore = TextEditingController();
+
+  void addstudent() {
+    final data = {
+      'studentname': stdname.text,
+      'course': stdcourse.text,
+      'score': stdscore.text,
+    };
+    students.add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,23 +41,26 @@ class _addstdState extends State<addstd> {
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: TextField(
-                decoration: InputDecoration(
+                controller: stdname,
+                decoration: const InputDecoration(
                     border: OutlineInputBorder(), label: Text("Student Name")),
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: TextField(
+                controller: stdcourse,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), label: Text("Course")),
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.all(8.0),
               child: TextField(
+                controller: stdscore,
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), label: Text("Score")),
@@ -51,7 +71,10 @@ class _addstdState extends State<addstd> {
                 backgroundColor: Colors.blue[900],
                 minimumSize: const Size(double.infinity, 50),
               ),
-              onPressed: () {},
+              onPressed: () {
+                addstudent();
+                Navigator.pop(context);
+              },
               child: const Text(
                 "Submit",
                 style: TextStyle(fontSize: 20, color: Colors.white),
--- lib/mentorscreen.dart
+++ lib/mentorscreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:scoreboardapp/addstudents.dart';
 
@@ -9,6 +10,9 @@ class Mentor_View extends StatefulWidget {
 }
 
 class _Mentor_ViewState extends State<Mentor_View> {
+  final CollectionReference students =
+      FirebaseFirestore.instance.collection('students');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +35,82 @@ class _Mentor_ViewState extends State<Mentor_View> {
         child: const Icon(Icons.add, color: Colors.white),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: StreamBuilder(
+          stream: students.snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot studentsSnap =
+                      snapshot.data.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 80,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                color: const Color.fromARGB(255, 206, 205, 205),
+                                blurRadius: 10,
+                                spreadRadius: 15)
+                          ]),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: 30,
+                                child: Text(studentsSnap['score'].toString()),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                studentsSnap['studentname'],
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                studentsSnap['course'],
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.edit,
+                                ),
+                                iconSize: 30,
+                                color: Colors.blue,
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.delete),
+                                iconSize: 30,
+                                color: Colors.red,
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return Container(); //set a lotti for no data
+          }),
     );
   }
 }
